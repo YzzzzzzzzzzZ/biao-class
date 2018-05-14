7e1d03f94c904686b4476510d26b83a81fa2a89e
Yz <326022924@qq.com>
2018-05-14 15:47:15 +0800

    添加了加载动画

--- project/004-serch-pagination-pack/js/tools/search.js
+++ project/004-serch-pagination-pack/js/tools/search.js
@@ -6,11 +6,20 @@
 *   userList: userList, //数据渲染的地方
 *   renderUserList: true // 是否在每次搜索时重置userlist
 * }, onSuccess: fn(), //搜索成功后执行的函数
-*   OnPageChangeSend: fn(), //页码改变时搜索成功后执行的函数
 *   method: method //方法
 *
 * */
-var page, obj, sendMethod, url;
+var page, obj, url, load;
+
+load = document.createElement('div');
+load.classList.add('loading');
+load.innerHTML = `
+<div>
+  <img src="img/load.png" class="load">
+</div>
+`;
+document.body.appendChild(load);
+load.hidden = true;
 
 var def = {
   keyword: '',
@@ -22,12 +31,15 @@ function searchUser(config, onSuccess, method) {
   obj = Object.assign({}, def, config);
 
   url = 'https://api.github.com/search/users?q=';
-  sendMethod = method || 'get';
 
-  send(obj, onSuccess, sendMethod);
+  load.hidden = false;
+
+  send(obj, onSuccess);
+
 }
 
 function send(obj, onSuccess, method) {
+  method = method || 'get';
   var http = new XMLHttpRequest();
   http.open(method, url + obj.keyword + '&page=' + obj.page + '&per_page=' + obj.limit);
   http.setRequestHeader('Authorization', 'Basic ' + btoa('YzzzzzzzzzzZ:28236ab9a5313fddca330f3791b1d002bace04e5'));
@@ -38,29 +50,15 @@ function send(obj, onSuccess, method) {
       obj.userList.innerHTML = '';
     }
     var data = JSON.parse(this.responseText);
+    load.hidden = true;
     onSuccess(data);
   });
 }
 
-function OnPageChangeSend(obj, onPageChange, method) {
-  method = method || sendMethod;
-  var http = new XMLHttpRequest();
-  http.open(method, url + obj.keyword + '&page=' + obj.page + '&per_page=' + obj.limit);
-  http.setRequestHeader('Authorization', 'Basic ' + btoa('YzzzzzzzzzzZ:28236ab9a5313fddca330f3791b1d002bace04e5'));
-  http.send();
-
-  http.addEventListener('load', function () {
-    if(obj.resetUserList) {
-      obj.userList.innerHTML = '';
-    }
-    var data = JSON.parse(this.responseText);
-    onPageChange(data);
-  });
-}
-
 function pageChanged(page, onPageChange) {
+  load.hidden = false;
   obj.page = page;
-  OnPageChangeSend(obj, onPageChange);
+  send(obj, onPageChange);
 }
 
 module.exports = {
